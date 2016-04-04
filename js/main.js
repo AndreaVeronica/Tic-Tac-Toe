edd994adb6348d64d26bc5393f97edb2e288539b
Andrea Airall <andreaairall@gmail.com>
2016-04-04 18:50:33 -0400

    winning solutions picked

--- js/main.js
+++ js/main.js
@@ -1,4 +1,3 @@
-
 //build game board
 // deply to gh-pages
 
@@ -12,24 +11,86 @@
 //build game board
 // deply to gh-pages
 
-//
-//
-//
+/* var moves = Array(9).join(".").split('.');
+ if (count % 2) {
+  moves[this.id]='x';
+}   */
 
-//
-//
-//
+$(document).ready(function() {
 
-$(document).ready(function(){
 
     var $gameCells = $('.squaresStyle');
 
+    //get array and counter
+    var moves = ["", "", "", "", "", "", "", "", ""];
+    var counter = 0;
+    var gameCountX = 0;
+    var gameCountO = 0;
+    var turn = 'x';
 
-    $($gameCells).one('click', function(){
-      console.log(this.id);
+
+// winning move ["x", "o", "", "x", "", "", "x", "o", ""]
+
+    $($gameCells).one('click', function() {
+        $(this).html(turn);
+        moves[this.id] = turn;
+
+        counter++;
+
+        if (counter % 2 === 0) {
+            turn = 'x';
+        } else {
+            turn = 'o';
+        }
+        // console.log(moves, counter,turn);
     });
 
 
+    $($gameCells).one('click', function(){
+
+    //test if winner found
+    if (moves[0] == 'x' && moves[1] =='x' && moves[2] == 'x' ||
+        moves[3] == 'x' && moves[4] =='x' && moves[5] == 'x' ||
+        moves[6] == 'x' && moves[7] =='x' && moves[8] == 'x' ||
+        moves[0] == 'x' && moves[3] =='x' && moves[6] == 'x' ||
+        moves[1] == 'x' && moves[4] =='x' && moves[5] == 'x' ||
+        moves[6] == 'x' && moves[7] =='x' && moves[8] == 'x' ||
+        moves[0] == 'x' && moves[4] =='x' && moves[8] == 'x' ||
+        moves[2] == 'x' && moves[4] =='x' && moves[6] == 'x' ) {
+          $('#winnerIs').html("Winner is X!");
+          //gameCountX++;
+          console.log(gameCountX);
+      } else if (moves[0] == 'o' && moves[1] =='o' && moves[2] == 'o' ||
+        moves[3] == 'o' && moves[4] =='o' && moves[5] == 'o' ||
+        moves[6] == 'o' && moves[7] =='o' && moves[8] == 'o' ||
+        moves[0] == 'o' && moves[3] =='o' && moves[6] == 'o' ||
+        moves[1] == 'o' && moves[4] =='o' && moves[5] == 'o' ||
+        moves[6] == 'o' && moves[7] =='o' && moves[8] == 'o' ||
+        moves[0] == 'o' && moves[4] =='o' && moves[8] == 'o' ||
+        moves[2] == 'o' && moves[4] =='o' && moves[6] == 'o' ) {
+          $('#winnerIs').html("Winner is O!");
+          //gameCountO++;
+
+
+      } else {
+
+         // $('#winnerIs').html("Draw!");
+      }
+
+
+     });
+
+     // function keepScore() {
+      //  if
+
+    //  }
+
+
+     // $('.resetGame').on('click', function(){
+
+     //  var empty = ["", "", "", "", "", "", "", "", ""];
+
+      //  });
 
 
 
